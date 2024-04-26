77c795943b8e76aa7eb274201f5f14b0ef97130f
Thomas Nguyen <thomse26@gmail.com>
2024-04-26 02:01:31 +0200

    modelloading

--- code/game/main.cpp
+++ code/game/main.cpp
@@ -4,11 +4,13 @@
 #include "Shader.h"
 #include "stb_image.h"
 #include "Camera.h"
+#include "tiny_gltf/tiny_gltf.h"
 #include <iostream>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 unsigned int loadTexture(char const * path);
@@ -93,6 +95,264 @@ float deltaTime = 0.0f;    // Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
 
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+bool loadModel(tinygltf::Model &model, const char *filename) {
+    tinygltf::TinyGLTF loader;
+    std::string err;
+    std::string warn;
+    bool res = loader.LoadASCIIFromFile(&model, &err, &warn, filename);
+    if(!res){
+        res = loader.LoadBinaryFromFile(&model, &err, &warn, filename);
+    }
+    if (!warn.empty()) {
+        std::cout << "WARN: " << warn << std::endl;
+    }
+
+    if (!err.empty()) {
+        std::cout << "ERR: " << err << std::endl;
+    }
+
+    if (!res)
+        std::cout << "Failed to load glTF: " << filename << std::endl;
+    else
+        std::cout << "Loaded glTF: " << filename << std::endl;
+
+    return res;
+}
+void bindMesh(std::map<int, GLuint>& vbos,tinygltf::Model &model, tinygltf::Mesh &mesh) {
+    for (size_t i = 0; i < model.bufferViews.size(); ++i) {
+        const tinygltf::BufferView &bufferView = model.bufferViews[i];
+        if (bufferView.target == 0) {  // TODO impl drawarrays
+            std::cout << "WARN: bufferView.target is zero" << std::endl;
+            continue;  // Unsupported bufferView.
+            /*
+              From spec2.0 readme:
+              https://github.com/KhronosGroup/glTF/tree/master/specification/2.0
+                       ... drawArrays function should be used with a count equal to
+              the count            property of any of the accessors referenced by the
+              attributes            property            (they are all equal for a given
+              primitive).
+            */
+        }
+
+        const tinygltf::Buffer &buffer = model.buffers[bufferView.buffer];
+        std::cout << "bufferview.target " << bufferView.target << std::endl;
+
+        GLuint vbo;
+        glGenBuffers(1, &vbo);
+        vbos[i] = vbo;
+        glBindBuffer(bufferView.target, vbo);
+
+        std::cout << "buffer.data.size = " << buffer.data.size()
+                  << ", bufferview.byteOffset = " << bufferView.byteOffset
+                  << std::endl;
+
+        glBufferData(bufferView.target, bufferView.byteLength,
+                     &buffer.data.at(0) + bufferView.byteOffset, GL_STATIC_DRAW);
+    }
+
+    for (size_t i = 0; i < mesh.primitives.size(); ++i) {
+        tinygltf::Primitive primitive = mesh.primitives[i];
+        tinygltf::Accessor indexAccessor = model.accessors[primitive.indices];
+
+        for (auto &attrib : primitive.attributes) {
+            tinygltf::Accessor accessor = model.accessors[attrib.second];
+            int byteStride =
+                    accessor.ByteStride(model.bufferViews[accessor.bufferView]);
+            glBindBuffer(GL_ARRAY_BUFFER, vbos[accessor.bufferView]);
+
+            int size = 1;
+            if (accessor.type != TINYGLTF_TYPE_SCALAR) {
+                size = accessor.type;
+            }
+
+            int vaa = -1;
+            if (attrib.first.compare("POSITION") == 0) vaa = 0;
+            if (attrib.first.compare("NORMAL") == 0) vaa = 1;
+            if (attrib.first.compare("TEXCOORD_0") == 0) vaa = 2;
+            if (vaa > -1) {
+                glEnableVertexAttribArray(vaa);
+                glVertexAttribPointer(vaa, size, accessor.componentType,
+                                      accessor.normalized ? GL_TRUE : GL_FALSE,
+                                      byteStride, BUFFER_OFFSET(accessor.byteOffset));
+            } else
+                std::cout << "vaa missing: " << attrib.first << std::endl;
+        }
+
+        if (model.textures.size() > 0) {
+            // fixme: Use material's baseColor
+            tinygltf::Texture &tex = model.textures[0];
+
+            if (tex.source > -1) {
+
+                GLuint texid;
+                glGenTextures(1, &texid);
+
+                tinygltf::Image &image = model.images[tex.source];
+                glBindTexture(GL_TEXTURE_2D, texid);
+                glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+                glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+                GLenum format = GL_RGBA;
+
+                if (image.component == 1) {
+                    format = GL_RED;
+                } else if (image.component == 2) {
+                    format = GL_RG;
+                } else if (image.component == 3) {
+                    format = GL_RGB;
+                } else {
+                    // ???
+                }
+
+                GLenum type = GL_UNSIGNED_BYTE;
+                if (image.bits == 8) {
+                    // ok
+                } else if (image.bits == 16) {
+                    type = GL_UNSIGNED_SHORT;
+                } else {
+                    // ???
+                }
+
+                glTexImage2D(GL_TEXTURE_2D, 0, format, image.width, image.height, 0,
+                             format, type, &image.image.at(0));
+            }
+        }
+    }
+}
+void bindModelNodes(std::map<int, GLuint>& vbos, tinygltf::Model &model,tinygltf::Node &node) {
+    if ((node.mesh >= 0) && (node.mesh < model.meshes.size())) {
+        bindMesh(vbos, model, model.meshes[node.mesh]);
+    }
+
+    for (size_t i = 0; i < node.children.size(); i++) {
+        assert((node.children[i] >= 0) && (node.children[i] < model.nodes.size()));
+        bindModelNodes(vbos, model, model.nodes[node.children[i]]);
+    }
+}
+std::pair<GLuint, std::map<int, GLuint>> bindModel(tinygltf::Model &model) {
+    std::map<int, GLuint> vbos;
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    const tinygltf::Scene &scene = model.scenes[model.defaultScene];
+    for (size_t i = 0; i < scene.nodes.size(); ++i) {
+        assert((scene.nodes[i] >= 0) && (scene.nodes[i] < model.nodes.size()));
+        bindModelNodes(vbos, model, model.nodes[scene.nodes[i]]);
+    }
+
+    glBindVertexArray(0);
+    // cleanup vbos but do not delete index buffers yet
+    for (auto it = vbos.cbegin(); it != vbos.cend();) {
+        tinygltf::BufferView bufferView = model.bufferViews[it->first];
+        if (bufferView.target != GL_ELEMENT_ARRAY_BUFFER) {
+            glDeleteBuffers(1, &vbos[it->first]);
+            vbos.erase(it++);
+        }
+        else {
+            ++it;
+        }
+    }
+
+    return {vao, vbos};
+}
+void drawMesh(const std::map<int, GLuint>& vbos,tinygltf::Model &model, tinygltf::Mesh &mesh) {
+    for (size_t i = 0; i < mesh.primitives.size(); ++i) {
+        tinygltf::Primitive primitive = mesh.primitives[i];
+        tinygltf::Accessor indexAccessor = model.accessors[primitive.indices];
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbos.at(indexAccessor.bufferView));
+
+        glDrawElements(primitive.mode, indexAccessor.count,
+                       indexAccessor.componentType,
+                       BUFFER_OFFSET(indexAccessor.byteOffset));
+    }
+}
+void drawModelNodes(const std::pair<GLuint, std::map<int, GLuint>>& vaoAndEbos,tinygltf::Model &model, tinygltf::Node &node) {
+    if ((node.mesh >= 0) && (node.mesh < model.meshes.size())) {
+        drawMesh(vaoAndEbos.second, model, model.meshes[node.mesh]);
+    }
+    for (size_t i = 0; i < node.children.size(); i++) {
+        drawModelNodes(vaoAndEbos, model, model.nodes[node.children[i]]);
+    }
+}
+void drawModel(const std::pair<GLuint, std::map<int, GLuint>>& vaoAndEbos,tinygltf::Model &model) {
+    glBindVertexArray(vaoAndEbos.first);
+
+    const tinygltf::Scene &scene = model.scenes[model.defaultScene];
+    for (size_t i = 0; i < scene.nodes.size(); ++i) {
+        drawModelNodes(vaoAndEbos, model, model.nodes[scene.nodes[i]]);
+    }
+
+    glBindVertexArray(0);
+}
+
+void dbgModel(tinygltf::Model &model) {
+    for (auto &mesh : model.meshes) {
+        std::cout << "mesh : " << mesh.name << std::endl;
+        for (auto &primitive : mesh.primitives) {
+            const tinygltf::Accessor &indexAccessor =
+                    model.accessors[primitive.indices];
+
+            std::cout << "indexaccessor: count " << indexAccessor.count << ", type "
+                      << indexAccessor.componentType << std::endl;
+
+            tinygltf::Material &mat = model.materials[primitive.material];
+            for (auto &mats : mat.values) {
+                std::cout << "mat : " << mats.first.c_str() << std::endl;
+            }
+
+            for (auto &image : model.images) {
+                std::cout << "image name : " << image.uri << std::endl;
+                std::cout << "  size : " << image.image.size() << std::endl;
+                std::cout << "  w/h : " << image.width << "/" << image.height
+                          << std::endl;
+            }
+
+            std::cout << "indices : " << primitive.indices << std::endl;
+            std::cout << "mode     : "
+                      << "(" << primitive.mode << ")" << std::endl;
+
+            for (auto &attrib : primitive.attributes) {
+                std::cout << "attribute : " << attrib.first.c_str() << std::endl;
+            }
+        }
+    }
+}
+std::pair<GLuint, std::map<int, GLuint>> loadGLTF(char const*filename){
+    tinygltf::Model model;
+    bool success = loadModel(model,filename);
+    if(!success){std::cout<<"Error loading model "<<filename<<std::endl;}
+
+    std::pair<GLuint, std::map<int, GLuint>> vaoAndEbos = bindModel(model);
+    dbgModel(model);
+    return vaoAndEbos;
+
+}
+
+
+
+//
+//tinygltf::Model gltfmodel;
+//std::pair<GLuint, std::map<int, GLuint>> vaoAndEbos;
+//bool success = loadModel(gltfmodel,filename.c_str());
+//if(!success){std::cout<<"Error loading model "<<filename<<std::endl;}
+
+//vaoAndEbos = bindModel(gltfmodel);
+//dbgModel(gltfmodel);
+
+
+
+//drawModel(vaoAndEbos,gltfmodel);
+
+//glDeleteVertexArrays(1, &vaoAndEbos.first);
+
+
+
 int main() {
     // glfw: initialize and configure
     // ------------------------------
@@ -129,11 +389,11 @@ int main() {
 
     // build and compile our shader program
     // ------------------------------------
-    Shader lightingShader("C:\\Users\\Thoms\\Documents\\_Studium\\S6\\OpenGL\\code\\assets\\vertexShader.vert",
-                     "C:\\Users\\Thoms\\Documents\\_Studium\\S6\\OpenGL\\code\\assets\\TextureFrag.frag"
+    Shader lightingShader("assets/vertexShader.vert",
+                     "assets/TextureFrag.frag"
     );
-    Shader lightCubeShader("C:\\Users\\Thoms\\Documents\\_Studium\\S6\\OpenGL\\code\\assets\\vertexShader.vert",
-                           "C:\\Users\\Thoms\\Documents\\_Studium\\S6\\OpenGL\\code\\assets\\fragementShader.frag"
+    Shader lightCubeShader("assets/vertexShader.vert",
+                           "assets/fragementShader.frag"
     );
 
     unsigned int VBO, cubeVAO;
@@ -164,12 +424,19 @@ int main() {
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    unsigned int diffuseMap = loadTexture("C:\\Users\\Thoms\\Documents\\_Studium\\S6\\OpenGL\\code\\assets\\Texture\\container2.png");
-    unsigned int specularMap = loadTexture("C:\\Users\\Thoms\\Documents\\_Studium\\S6\\OpenGL\\code\\assets\\Texture\\container2_specular.png");
+    unsigned int diffuseMap = loadTexture("assets/Texture/container2.png");
+    unsigned int specularMap = loadTexture("assets/Texture/container2_specular.png");
+    unsigned int shipTextureD = loadTexture("assets/Texture/ship_Diffuse.png");
     lightingShader.use();
     lightingShader.setInt("material.diffuse", 0);
     lightingShader.setInt("material.specular", 1);
 
+    tinygltf::Model gltfmodel;
+    std::string filename =  "assets/models/Ship_TopDown.glb";
+    std::pair<GLuint, std::map<int, GLuint>> vaoAndEbos;
+    bool success = loadModel(gltfmodel,filename.c_str());
+    dbgModel(gltfmodel);
+    vaoAndEbos = bindModel(gltfmodel);
     while (!glfwWindowShouldClose(window)) {
         float currentFrame = static_cast<float>(glfwGetTime());
         deltaTime = currentFrame - lastFrame;
@@ -251,13 +518,16 @@ int main() {
         glm::mat4 model = glm::mat4(1.0f);
         lightingShader.setMat4("model", model);
 
+
         // bind diffuse map
-        glActiveTexture(GL_TEXTURE0);
+        glActiveTexture(GL_TEXTURE0+diffuseMap);
         glBindTexture(GL_TEXTURE_2D, diffuseMap);
         // bind specular map
-        glActiveTexture(GL_TEXTURE1);
+        glActiveTexture(GL_TEXTURE0+specularMap);
         glBindTexture(GL_TEXTURE_2D, specularMap);
         glBindVertexArray(cubeVAO);
+
+        drawModel(vaoAndEbos, gltfmodel);
 
         for (unsigned int i = 0; i < 10; i++)
         {
@@ -305,6 +575,7 @@ int main() {
     glDeleteVertexArrays(1, &cubeVAO);
     glDeleteVertexArrays(1, &lightCubeVAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &vaoAndEbos.first);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
